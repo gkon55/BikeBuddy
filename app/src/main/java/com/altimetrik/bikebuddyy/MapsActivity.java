8062e4399027e08c3332ede12b3bbf2058dd8e81
Girish Kumar Kontham <girish.kontham@gmail.com>
2018-06-21 13:06:00 -0500

    main comitt

--- app/src/main/java/com/altimetrik/bikebuddyy/MapsActivity.java
+++ app/src/main/java/com/altimetrik/bikebuddyy/MapsActivity.java
@@ -1,8 +1,8 @@
 package com.altimetrik.bikebuddyy;
 
-import android.support.v4.app.FragmentActivity;
+import android.location.Location;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +10,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
+    private LatLng nycLatLong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +24,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        nycLatLong = new LatLng(40.7, 74.0);
+
+        mMap.addMarker(new MarkerOptions().position(nycLatLong).title("Your Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(nycLatLong));
     }
+
+    public double distanceCalculator(LatLng destinationLatLng){
+
+        Location startPoint=new Location("locationA");
+        startPoint.setLatitude(nycLatLong.latitude);
+        startPoint.setLongitude(nycLatLong.longitude);
+
+        Location endPoint=new Location("locationA");
+        endPoint.setLatitude(destinationLatLng.latitude);
+        endPoint.setLongitude(destinationLatLng.longitude);
+
+        double distance = startPoint.distanceTo(endPoint);
+
+        return distance;
+    }
+
 }
